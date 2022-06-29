b2cc333498168901174e39db8cc53b6bfd9570a6
DESKTOP-P2E4GPS\505 <hexun_@naver.com>
2022-06-29 12:17:13 +0900

    2022_06_29 js

---
+++ 0629/test02.js
@@ -0,0 +1,6 @@
+// 문자열 길이
+const arr = ['short', 'long sentence, it is not appropriate'];
+
+arr.forEach(str=>{
+    if(str.length<10) console.log(str);
+});
---
+++ 0629/test09.js
@@ -0,0 +1,7 @@
+// 배열 요소 제거
+const arr = [1,2,3];
+
+console.log(arr.pop());
+console.log(arr.pop());
+console.log(arr.pop());
+console.log(arr.pop());
